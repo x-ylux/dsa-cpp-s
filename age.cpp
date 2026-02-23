8620c44fa8af2c89b27cdd5c243a1ac8c879ae03
x-ylux <ms7479213@gmail.com>
2026-02-23 16:15:53 +0530

    checking whether someone is adult or not

---
+++ age.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main () { 
+    int age ;
+    cin >> age ;
+    if (age >=18) {
+        cout << "you are an adult";
+
+    }
+         else {
+            cout<<"you are not an adult";
+         }
+         return 0;
+    }
+    
+    
