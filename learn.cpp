f604e2794734b5253bfb67e6d6c09a1941ce1f0a
x-ylux <ms7479213@gmail.com>
2026-02-23 15:23:16 +0530

    basic

---
+++ learn.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main () {
+    // string to take anthing before the space s1 will do that and s2 will take the rest of the string after the space
+   //string s1 , s2;
+   // cin >>s1 >>s2 ;
+  // cout << s1 <<" "<<  s2 ;
+
+  //to take the whole line as input we use getline function
+ // string hms ;
+ // getline (cin ,  hms);
+//  cout << hms;
+ 
+// char to take a single character as an input 
+char mh ;
+cin>> mh;
+cout << mh;
+      return 0;
+    
+}
