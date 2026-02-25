c85a1e1040f6d7f3c664461a07ab5fd8056cfcca
x-ylux <ms7479213@gmail.com>
2026-02-25 16:44:16 +0530

    strings

---
+++ string.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s= "mahi";
+    int len= s.size();
+    s [len-3]='j';
+    cout<< s[len-3];
+    return 0;
+
+}
+// array is defined for numbers and string is defined for character and same properties apply for string as it were for array
