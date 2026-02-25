ee193843dd0cff4ca6b9e1488cd8db76d00c36e6
x-ylux <ms7479213@gmail.com>
2026-02-25 15:58:29 +0530

    2 d arrays

---
+++ 2darray.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+// used to solve graphs and matrix problems 
+int main (){
+    int arr [3][5]; //it is denoting a matrix of 3 columns and 5 rows
+    arr[1][3]= 22;
+    cout<< arr[1][4];/*it will give diff values everytime beacuse only fixed index is for 
+[1][3] other than this index if you write any other location for the index you will get different
+numbers everytime*/
+    return 0;
+
+}
