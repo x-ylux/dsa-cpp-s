4f2310913ea63d998f39de679abfabcc324e315a
x-ylux <ms7479213@gmail.com>
2026-02-26 22:34:00 +0530

    while loop

---
+++ dowhileloop.cpp
@@ -0,0 +1,14 @@
+/*when the condition is user dependent or we want the code to run 
+at least 1 time then we use do while loops*/
+#include<bits/stdc++.h>
+using namespace std;
+int main () {
+    int i =2;
+    do {
+        cout << "hey mahi" << i << endl;
+        i = i + 1;
+    } while (i<=1);
+
+
+    return 0;
+}
