b8f4ab4a571e0868f417389a3e09c78aa55700c7
x-ylux <ms7479213@gmail.com>
2026-02-26 22:17:57 +0530

    whileloop

---
+++ whileloop.cpp
@@ -0,0 +1,13 @@
+/*in while loop logic remains the same but only syntax changes */
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int i =1;
+    while(i<=5){
+        cout<<"hey mahi"<<i<<endl;
+
+        i = i + 1;
+    }
+    return 0;
+
+}
